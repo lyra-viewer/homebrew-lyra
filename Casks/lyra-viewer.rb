19b30ed7ad749f88e1ba75aca161b8d935dabc0f
nineveh <nb.nnvh@pm.me>
2026-02-28 18:57:21 +0100

    upgrade 0.3.0

--- Casks/lyra-viewer.rb
+++ Casks/lyra-viewer.rb
@@ -1,10 +1,10 @@
 cask "lyra-viewer" do
-  version "0.2.1"
+  version "0.3.0"
 
   arch arm: "arm64", intel: "x64"
 
-  sha256 arm:   "818dfc83cbc0ee3f4b4ade1a40dd94de110e976e9db47e4b0db95c09012e3a07",
-         intel: "ba58366984a255db5a7cc5cafb7ff9fdd603449641b8642761e7d5388a3afa24"
+  sha256 arm:   "6a7d61cc2faf2b08a6fcbf9c64b09bec6a9af53c3a7e5a6253c3b79a2293cfa7",
+         intel: "56bf39a65eaf15fa5c7a3e43e9cf83ffd67be88dcc151315e878dc0d08a054d7"
 
   url "https://github.com/lyra-viewer/Lyra/releases/download/v#{version}/LyraViewer-macos-#{arch}.zip"
   name "Lyra Viewer"
@@ -15,6 +15,7 @@ cask "lyra-viewer" do
   depends_on formula: "openexr"
   depends_on formula: "libheif"
   depends_on formula: "libde265"
+  depends_on formula: "openjpeg"
 
   # Zips contain: LyraViewer-arm.app / LyraViewer-intel.app
   app "LyraViewer-#{arch}.app", target: "LyraViewer.app"
@@ -23,5 +24,7 @@ cask "lyra-viewer" do
   "~/Library/Preferences/com.nineveh.lyraviewer.plist",
   "~/Library/Saved Application State/com.nineveh.lyraviewer.savedState",
   "~/.local/share/LyraViewer",
+  "~/.config/lyra-viewer",
+  "~/.local/share/lyra-viewer",
   ]
 end
