a48574b0a57d678eec2979c5c70c1aff9b3bd0c7
nineveh <nb.nnvh@pm.me>
2026-03-20 19:44:45 +0100

    upgrade 0.3.3

--- Casks/lyra-viewer.rb
+++ Casks/lyra-viewer.rb
@@ -1,10 +1,10 @@
 cask "lyra-viewer" do
-  version "0.3.0"
+  version "0.3.3"
 
   arch arm: "arm64", intel: "x64"
 
-  sha256 arm:   "6a7d61cc2faf2b08a6fcbf9c64b09bec6a9af53c3a7e5a6253c3b79a2293cfa7",
-         intel: "56bf39a65eaf15fa5c7a3e43e9cf83ffd67be88dcc151315e878dc0d08a054d7"
+  sha256 arm:   "3317507f9c62ebc81fda0ab7b8d46f0edf049dd80a9a4333fa99a7ee3f112a27",
+         intel: "9d2f39f53e61ddb67d42b1dc04a971f10cb76ccb8ebca589048fa65cc6a74cb3"
 
   url "https://github.com/lyra-viewer/Lyra/releases/download/v#{version}/LyraViewer-macos-#{arch}.zip"
   name "Lyra Viewer"
